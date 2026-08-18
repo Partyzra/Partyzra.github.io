8c2b0e203afca8394d63f903abee3ba99bd08f3a
Partyzra <12rparker@gmail.com>
2026-08-18 10:05:39 -0600

    Update photos.js

--- js/photos.js
+++ js/photos.js
@@ -2,6 +2,8 @@
 // Add a new entry here whenever you add a photo to Images/photo-full/.
 const PORTFOLIO_PHOTOS = [
   { file: 'Abandoned.JPG', caption: 'Abandoned' },
+  { file: 'Abandoned1.jpg', caption: 'Abandoned1' },
+{ file: 'Abandoned2.jpg', caption: 'Abandoned2' },
   { file: 'Andrew.JPG', caption: 'Andrew' },
   { file: 'Barn.jpg', caption: 'Barn' },
   { file: 'Barnstreet.jpg', caption: 'Barnstreet' },
@@ -36,7 +38,71 @@ const PORTFOLIO_PHOTOS = [
   { file: 'Sunset.jpg', caption: 'Sunset' },
   { file: 'The Rocket.jpg', caption: 'The Rocket' },
   { file: 'Weed.JPG', caption: 'Weed' },
-  { file: 'Zeboy!.jpg', caption: 'Zeboy!' }
+  { file: 'Zeboy!.jpg', caption: 'Zeboy!' },
+{ file: 'Contrast.jpg', caption: 'Contrast' },
+{ file: 'Cornfield.jpg', caption: 'Cornfield' },
+{ file: 'Cow Charging.jpg', caption: 'Cow Charging' },
+{ file: 'Creepy Derick.jpg', caption: 'Creepy Derick' },
+{ file: 'Derick.jpg', caption: 'Derick' },
+{ file: 'Derick2.jpg', caption: 'Derick2' },
+{ file: 'Derick3.jpg', caption: 'Derick3' },
+{ file: 'Dori is Wednesday.jpg', caption: 'Dori is Wednesday' },
+{ file: 'Dori.jpg', caption: 'Dori' },
+{ file: 'Fence.jpg', caption: 'Fence' },
+{ file: 'Field.jpg', caption: 'Field' },
+{ file: 'Flowers.jpg', caption: 'Flowers' },
+{ file: 'Friends.jpg', caption: 'Friends' },
+{ file: 'Friends.jpg', caption: 'Friends' },
+{ file: 'Gargoyle.jpg', caption: 'Gargoyle' },
+{ file: 'Grasshopper.png', caption: 'Grasshopper.png' },
+{ file: 'Hawaii Beach.jpg', caption: 'Hawaii Beach' },
+{ file: 'Hawaii Couple.jpg', caption: 'Hawaii Couple' },
+{ file: 'Hawaii Girl.jpg', caption: 'Hawaii Girl' },
+{ file: 'Hawaii Mountain.jpg', caption: 'Hawaii Mountain' },
+{ file: 'Hawaii Water Crash.jpg',caption: 'Hawaii Water Crash'},
+{ file: 'Helmets.jpg', caption: 'Helmets' },
+{ file: 'Horses.jpg', caption: 'Horses' },
+{ file: 'In The Rain.jpg', caption: 'In The Rain' },
+{ file: 'Justin.jpg', caption: 'Justin' },
+{ file: 'Lily.jpg', caption: 'Lily' },
+{ file: 'Lily2.jpg', caption: 'Lily2' },
+{ file: 'Me.jpg', caption: 'Me' },
+{ file: 'My Father.jpg', caption: 'My Father' },
+{ file: 'My Father2.jpg', caption: 'My Father2' },
+{ file: 'My Father3.jpg', caption: 'My Father3' },
+{ file: 'My Mother.jpg', caption: 'My Mother' },
+{ file: 'Night Car.jpg', caption: 'Night Car' },
+{ file: 'Night Sky.jpg', caption: 'Night Sky' },
+{ file: 'Open Mouth.jpg', caption: 'Open Mouth' },
+{ file: 'Peace.jpg', caption: 'Peace' },
+{ file: 'Peacock.jpg', caption: 'Peacock' },
+{ file: 'Possey.jpg', caption: 'Possey' },
+{ file: 'Printing Press3.jpg', caption: 'Printing Press3' },
+{ file: 'Rattlesnake Rapids.jpg', caption: 'Rattlesnake Rapids' },
+{ file: 'Weed.jpg', caption: 'Weed' },
+
+
+
+
+
+
+
+
+
+Performance
+Phoebe
+
+
+
+
+Seagull
+Sean
+Sun Kitty
+Sunflowers
+Sunset
+The Rocket
+Weed
+Zeboy!
 ];
 
 (function renderPhotographyGallery() {
