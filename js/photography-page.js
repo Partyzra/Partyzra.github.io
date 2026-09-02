88c96b06eb9221e10edeec31d731f154a0c5ad08
Partyzra <12rparker@gmail.com>
2026-09-01 23:19:18 -0600

    deleted/changed

--- js/photography-page.js
+++ js/photography-page.js
@@ -263,11 +263,7 @@
     if (stem === 'miss watson') return '50% 15%';      // head near top
     if (stem === 'fox1') return '50% 10%';      // head near top
     if (stem === 'barnstreet') return '10% 60%';      // to the left
-
     if (stem === 'seagull1') return '100% 50%';      // to the right
-
-        if (stem === 'horses') return '0% 60%';      // to the left
-
 
   };
 
